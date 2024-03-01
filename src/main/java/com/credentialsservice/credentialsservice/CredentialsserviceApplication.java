2d6a0c3bf2ee0408370525437d1dd01a286924c8
plmaskw2 <maciek115606@gmail.comn>
2024-03-01 03:15:59 +0100

    Initial commit

---
+++ src/main/java/com/credentialsservice/credentialsservice/CredentialsserviceApplication.java
@@ -0,0 +1,13 @@
+package com.credentialsservice.credentialsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredentialsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredentialsserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/credentialsservice/credentialsservice/CredentialsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credentialsservice.credentialsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredentialsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
